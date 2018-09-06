e0d0ba1a10abb2602c1c72742641406fd504e0a9
TeresaG <tmg.coder@gmail.com>
2018-09-06 16:34:01 -0700

    changed friends to find

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,49 +1,81 @@
 var friendArray = [
     {
-        "name":"Luke",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name":"Rachel Green",
+        "photo":"https://vignette.wikia.nocookie.net/friends/images/3/38/RachelGreen.jpg/revision/latest/scale-to-width-down/350?cb=20180426182043",
         "scores":[
-            5,
-            1,
+            2,
+            2,
             4,
+            5,
+            3,
+            5,
+            5,
             4,
             5,
-            1,
+            5
+          ]
+      },
+      {
+        "name":"Ross Geller",
+        "photo":"https://vignette.wikia.nocookie.net/friends/images/0/0b/RossGeller.jpg/revision/latest/scale-to-width-down/350?cb=20180424154547",
+        "scores":[
             2,
             5,
+            1,
             4,
+            2,
+            2,
+            2,
+            1,
+            5,
             1
           ]
       },
       {
-        "name":"Vader",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name":"Phoebe Buffay",
+        "photo":"https://vignette.wikia.nocookie.net/friends/images/3/30/PhoebeBuffay.jpg/revision/latest/scale-to-width-down/350?cb=20180426182547",
         "scores":[
-            5,
-            1,
-            1,
             4,
-            2,
             1,
             2,
             1,
             5,
+            5,
+            4,
+            1,
+            1,
             1
           ]
       },
       {
-        "name":"Han Solo",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name":"Joey Tribbiani",
+        "photo":"https://vignette.wikia.nocookie.net/friends/images/f/f5/JoeyTribbiani.jpg/revision/latest/scale-to-width-down/350?cb=20180424154245",
         "scores":[
             2,
+            3,
+            3,
             1,
+            1,
+            1,
+            3,
+            5,
+            5,
+            5
+          ]
+      },
+      {
+        "name":"Monica Geller",
+        "photo":"https://vignette.wikia.nocookie.net/friends/images/2/2f/Monica_Geller-Bing_Season_10.png/revision/latest/scale-to-width-down/350?cb=20180426182234",
+        "scores":[
+            2,
+            5,
             4,
-            2,
+            5,
             1,
             1,
             2,
-            1,
-            1,
+            5,
+            5,
             1
           ]
       }
